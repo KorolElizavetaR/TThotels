5d5633ec241c3b1fd2821bed860bfaea47014df3
KorolER <elizavetakorol2005@gmail.com>
2026-03-03 19:38:04 +0300

    first commit

---
+++ src/main/java/koroler/TThotels/TThotelsApplication.java
@@ -0,0 +1,13 @@
+package koroler.TThotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TThotelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TThotelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/koroler/TThotels/TThotelsApplicationTests.java
@@ -0,0 +1,13 @@
+package koroler.TThotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TThotelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
